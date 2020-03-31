e86e442a6d72f72152a42109a1e2557e86b84619
Phil McDaniel <mcdanipc@gmail.com>
2020-03-31 14:47:02 -0500

    PCM add plotting for US cases & Italy cases

--- COVID-19.py
+++ COVID-19.py
@@ -2,11 +2,39 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 #read csv into dataframe
-data = pd.read_csv(r'C:\Users\mcdan\Documents\GitHub\COVID-19\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_global.csv')
+data = pd.read_csv(r'C:\Users\phil_\OneDrive\Documents\GitHub\COVID-19-DATA\csse_covid_19_data\csse_covid_19_time_series\time_series_covid19_confirmed_global.csv')
 
-#filter datafrom for USA
-usd = data[data["Country/Region"]=="US"]
-usd
+#filter datafrom for USA & Italy
+us = data[data["Country/Region"]=="US"]
+italy = data[data["Country/Region"]=="Italy"]
+
+#move columns to rows using melt
+pvtus = pd.melt(us,id_vars = ['Province/State', 'Country/Region', 'Lat', 'Long'],var_name = 'Date',value_name = 'Cases')
+pvtitaly = pd.melt(italy,id_vars = ['Province/State', 'Country/Region', 'Lat', 'Long'],var_name = 'Date',value_name = 'Cases')
+#pvtus
+
+#convert date column to date datatype
+pvtus["Date"] = pd.to_datetime(pvtus["Date"])
+pvtitaly["Date"] = pd.to_datetime(pvtitaly["Date"])
+
+
+
+#get just the dates
+datesus = pvtus['Date']
+datesitaly = pvtitaly['Date']
+
+#get just the values
+valus = pvtus['Cases']
+valitaly = pvtitaly['Cases']
+
+plt.plot(datesus.values,valus.values,label = 'US Cases')
+plt.plot(datesitaly.values,valitaly.values,label = 'Italy Cases')
+plt.xlabel('Date')
+plt.ylabel('Cases')
+
+plt.legend()
+plt.show()
 
