26df714a2b2658b2ff4fbe522656b0cda7e76c60
Phil McDaniel <mcdanipc@gmail.com>
2020-12-18 10:50:33 -0600

    Added annotates on daily cases

--- dailycases.py
+++ dailycases.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
 
@@ -25,9 +26,53 @@ rolleddata = data.groupby(['Country_Region','Date'],as_index=False).sum('Cases')
 rolleddata['New Cases'] = rolleddata.Cases.diff()
 #rolleddata
 
+# add column that is date integer
+rolleddata['Date int'] = mdates.date2num(rolleddata['Date'])
+#rolleddata
+
+# get important dates for annotation
+thanksgiving = rolleddata[rolleddata['Date'] =='2020-11-26']
+nationalemergency = rolleddata[rolleddata['Date'] =='2020-03-13']
+firstvaccination = rolleddata[rolleddata['Date'] =='2020-12-14']
+memorialday = rolleddata[rolleddata['Date'] =='2020-05-25']
+laborday = rolleddata[rolleddata['Date'] =='2020-09-07']
+
 plt.bar(rolleddata['Date'],rolleddata['New Cases'],align='center')
 plt.xlabel('Date')
 plt.ylabel('New Cases')
 plt.ticklabel_format(axis="y",style="plain")
 plt.title("Daily New Cases - US")
+# https://matplotlib.org/3.1.1/gallery/text_labels_and_annotations/annotation_demo.html
+plt.annotate('Thanksgiving'
+            ,xy=(thanksgiving['Date int'],0),xycoords = 'data'
+            ,xytext=(0,-80),textcoords='offset pixels'
+            ,arrowprops=dict(facecolor='black', shrink=0.05,width = 1,headwidth = 8)
+            ,horizontalalignment='center', verticalalignment='top'
+            )
+plt.annotate('National Emergency Declared'
+            ,xy=(nationalemergency['Date int'],0),xycoords = 'data'
+            ,xytext=(0,-100),textcoords='offset pixels'
+            ,arrowprops=dict(facecolor='black', shrink=0.05,width = 1,headwidth = 8)
+            ,horizontalalignment='center', verticalalignment='top'
+            )
+plt.annotate('First US Vaccination'
+            ,xy=(firstvaccination['Date int'],0),xycoords = 'data'
+            ,xytext=(0,-100),textcoords='offset pixels'
+            ,arrowprops=dict(facecolor='black', shrink=0.05,width = 1,headwidth = 8)
+            ,horizontalalignment='center', verticalalignment='top'
+            )
+plt.annotate('Memorial Day'
+            ,xy=(memorialday['Date int'],0),xycoords = 'data'
+            ,xytext=(0,-100),textcoords='offset pixels'
+            ,arrowprops=dict(facecolor='black', shrink=0.05,width = 1,headwidth = 8)
+            ,horizontalalignment='center', verticalalignment='top'
+            )
+plt.annotate('Labor Day'
+            ,xy=(laborday['Date int'],0),xycoords = 'data'
+            ,xytext=(0,-100),textcoords='offset pixels'
+            ,arrowprops=dict(facecolor='black', shrink=0.05,width = 1,headwidth = 8)
+            ,horizontalalignment='center', verticalalignment='top'
+            )
+
+
 plt.show()
