b202ea93600215e093731ac6c77a3500d98b5522
Phil McDaniel <mcdanipc@gmail.com>
2020-12-21 08:16:15 -0600

    fix the extra .0 that was showing up on annotation

--- dailycases.py
+++ dailycases.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import numpy as np
 
 url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
 
